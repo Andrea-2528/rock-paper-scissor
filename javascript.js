5c87b28fd6da97a49c3561a690e194b45a927d69
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-11-24 16:25:13 +0100

    Add logic for computer and user choice

--- javascript.js
+++ javascript.js
@@ -3,17 +3,29 @@
 function getComputerChoice () {
     let num = Math.random();
     if (num <= 0.3) {
-        return "Rock";
+        return "rock";
     }
     else if (num > 0.3 && num <= 0.6) {
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissor";
+        return "scissors";
     }
 }
 
+console.log(getComputerChoice()); //debug
+
 function getHumanChoice () {
-    
+    let choice = prompt("Choose: rock, paper or scissors", "rock");
+    choice = choice.toLowerCase();
+    while (true){
+        if (choice == "rock" || choice == "paper" || choice == "scissors") {
+            return choice;
+        } else {
+            choice = prompt("The only accepted inputs are rock, paper and scissors.", "rock");
+        }
+    }    
 }
+
+console.log(getHumanChoice());  //debug
 

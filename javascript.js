61b901a53a75a7f23382e82124b34199ca0baf2c
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-11-26 12:59:48 +0100

    Add buttons logic to play a round

--- javascript.js
+++ javascript.js
@@ -1,33 +1,19 @@
-// The following game is played through the console.
-
 console.log("A new game begins!");
 
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice () {
-    let num = Math.random();
-    if (num <= 0.3) {
+    let num = Math.floor((Math.random())*3 + 1);
+    if (num == 1) {
         return "rock";
     }
-    else if (num > 0.3 && num <= 0.6) {
+    else if (num == 2) {
         return "paper";
     }
-    else {
+    else if (num == 3) {
         return "scissors";
     }
-}
-
-function getHumanChoice () {
-    let choice = prompt("Choose: rock, paper or scissors", "rock");
-    choice = choice.toLowerCase();
-    while (true){
-        if (choice == "rock" || choice == "paper" || choice == "scissors") {
-            return choice;
-        } else {
-            choice = prompt("The only accepted inputs are rock, paper and scissors.", "rock");
-        }
-    }    
 }
 
 function playRound (humanChoice,computerChoice) {
@@ -42,27 +28,71 @@ function playRound (humanChoice,computerChoice) {
     }
 }
 
-function playGame () {
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+
+rockButton.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paperButton.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissorsButton.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function playGame () {
     
-    console.log("Five rounds will be played:");
+//     console.log("Five rounds will be played:");
 
-    for (let i=0; i<5 ; i++) {
-        const computerSelection = getComputerChoice();
-        const humanSelection = getHumanChoice();
-        playRound(humanSelection, computerSelection);
-        console.log(`The score is: Human = ${humanScore} Computer = ${computerScore}`);
-    }
+//     for (let i=0; i<5 ; i++) {
+//         const computerSelection = getComputerChoice();
+//         console.log(computerSelection);
+//         const humanSelection = getHumanChoice();
+//         playRound(humanSelection, computerSelection);
+//         console.log(`The score is: Human = ${humanScore} Computer = ${computerScore}`);
+//     }
 
-    if (humanScore>computerScore) {
-        console.log("Congratulations!!! You won the game!");
-    }else if (humanScore<computerScore) {
-        console.log("What a shame!!! You lost the game!");
-    }else {
-        console.log("How boring... you tied...");
-    }
+//     if (humanScore>computerScore) {
+//         console.log("Congratulations!!! You won the game!");
+//     }else if (humanScore<computerScore) {
+//         console.log("What a shame!!! You lost the game!");
+//     }else {
+//         console.log("How boring... you tied...");
+//     }
 
-}
+// }
 
-playGame();
-
-console.log("Refresh the page to play again!");
+// playGame();
